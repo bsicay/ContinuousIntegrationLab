c9c71aa57b3ac2b546cf16012ceba41ec42e9654
bsicay10 <bsicay@pacifiko.com>
2025-09-10 18:06:43 -0600

    Remove TEST BREAK

--- test_math_lib.py
+++ test_math_lib.py
@@ -127,8 +127,7 @@ class TestMathLib(unittest.TestCase):
         Esta prueba está diseñada para fallar y demostrar que el sistema de CI funciona.
         """
         # Esta línea está intencionalmente mal para demostrar que los tests fallan
-        # Comenta la siguiente línea para que los tests pasen
-        self.assertEqual(square(2), 5)  # Esto debería ser 4, no 5
+        # self.assertEqual(square(2), 5)  # Esto debería ser 4, no 5
 
 
 if __name__ == '__main__':
